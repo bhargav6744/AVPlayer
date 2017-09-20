5ea90dfcf507eb917aadc77344ffc46115b1ce19
Bhargav Sejpal <sejpalbhargav67@gmail.com>
2017-09-20 18:28:16 +0530

    AVPlayer

---
+++ AVPlayer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AVPlayer
+//
+//  Created by Hitesh V-PI on 12/06/17.
+//  Copyright © 2017 Pixabit Infotech. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
